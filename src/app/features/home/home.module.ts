da190a0b631a58b3a2bc7989f2eed6c3d5e34c69
Andrea Lucia Contreras Díaz <andrea.contreras.-nd@disney.com>
2023-06-27 20:58:14 -0400

    feat: adding pokemon list.
    fix(styles/variables): using material naming convention.
    fix(core): folders.
    fix(shared): folders.

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
-import { SharedModule } from 'src/app/shared/shared/shared.module'
+import { SharedModule } from 'src/app/shared/shared.module'
 
 import { HomeRoutingModule } from './home-routing.module'
 import { HomeComponent } from './home.component'
--- src/app/features/list/list.module.ts
+++ src/app/features/list/list.module.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
-import { SharedModule } from 'src/app/shared/shared/shared.module'
-
+import { SharedModule } from 'src/app/shared/shared.module'
 import { ListRoutingModule } from './list-routing.module'
 import { ListComponent } from './list.component'
 
