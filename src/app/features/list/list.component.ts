b7f12c5ea04fe78381262a39fdf515d67727ae3a
Andrea Lucia Contreras Díaz <andrea.contreras.-nd@disney.com>
2023-06-27 22:27:14 -0400

    feat(list): dynamic list.
    feat(list): mark as favourite.

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
 
+interface PokemonModel {
+  id: number;
+  name: string;
+  favourite?: boolean;
+}
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent {
+  public pokemonList: PokemonModel[] = [
+    {
+      id: 35,
+      name: "clefairy",
+      favourite: false
+    },
+    {
+      id: 36,
+      name: "bulbasaur",
+      favourite: false
+    },
+    {
+      id: 37,
+      name: "ivysaur",
+      favourite: false
+    },
+    {
+      id: 38,
+      name: "venasaur",
+      favourite: false
+    }
+  ];
+  public pokemonFavouriteList: PokemonModel[] = [];
 
+  public showFavourites = false;
+
+  public onAddToFavourites(id: number): void {
+    this.pokemonList = this.pokemonList.map((pokemon: PokemonModel) => {
+      if (pokemon.id === id) {
+        this.pokemonFavouriteList.push(pokemon);
+        pokemon.favourite = !pokemon.favourite;
+      }
+
+      return pokemon;
+    });
+  }
 }
